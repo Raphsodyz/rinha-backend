2751ad03c4e15d5efa0b5723e71351fe7fb68d45
Dev02ProAuto <dev02@lplsolutions.com.br>
2023-09-25 15:23:12 -0300

    Fix na connection string && verbatim string

--- rinha-backend/Context/RinhaContext.cs
+++ rinha-backend/Context/RinhaContext.cs
@@ -9,7 +9,7 @@ namespace rinha_backend.Context
     public class RinhaContext
     {
         public static string ConnectionString(){
-            return "Server=localhost;Port=5432;Database=rinha;User Id=postgres;Password=;" +
+            return "Host=localhost;Port=5432;Database=jajaj;User ID=postgres;Password=root;" +
                 "Pooling=true;MinPoolSize=1;MaxPoolSize=1024;";
         }
 
@@ -18,28 +18,28 @@ namespace rinha_backend.Context
         }
 
         public static string GetParam(string nome, string apelido, string stack){
-            string query = "SELECT * FROM PESSOA WHERE 1=1 ";
+            string query = @"SELECT * FROM PESSOA WHERE 1=1 ";
             
             if(!string.IsNullOrWhiteSpace(nome))
-                query += "AND NOME ILIKE @nome";
+                query += @"AND NOME ILIKE @nome ";
             else if(!string.IsNullOrWhiteSpace(apelido))
-                query += "AND APELIDO ILIKE @apelido";
+                query += @"AND APELIDO ILIKE @apelido ";
             else if(!string.IsNullOrWhiteSpace(stack))
-                query += "AND STACK ILIKE @stack";
+                query += @"AND STACK ILIKE @stack ";
             else
                 throw new ArgumentNullException();
             
-            query += "LIMIT 50";
+            query += @"LIMIT 50";
             return query;
         }
 
         public static string Count(){
-            return @"SELECT COUNT(ID) FROM PESSOA";
+            return @"SELECT COUNT(1) FROM PESSOA";
         }
 
         public static string Post(Pessoa pessoa){
-            return "INSERT INTO PESSOA (ID, NOME, APELIDO, NASCIMENTO, STACK)" + 
-                    "VALUES (@Id, @Nome, @Apelido, @Nascimento, @Stack) RETURNING ID";
+            return @"INSERT INTO PESSOA (ID, NOME, APELIDO, NASCIMENTO, STACK)" + 
+                    @"VALUES (@Id, @Nome, @Apelido, @Nascimento, @Stack) RETURNING ID";
         }
     }
 }
