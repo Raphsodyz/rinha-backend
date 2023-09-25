641aed6ac4f5413cd8f0459535a583dc1c62ecee
Dev02ProAuto <dev02@lplsolutions.com.br>
2023-09-25 15:23:30 -0300

    forget staging haha

--- rinha-backend/Context/RinhaContext.cs
+++ rinha-backend/Context/RinhaContext.cs
@@ -14,32 +14,32 @@ namespace rinha_backend.Context
         }
 
         public static string Get(Guid id){
-            return @"SELECT * FROM PESSOA WHERE ID = @id"; 
+            return "SELECT * FROM PESSOA WHERE ID = @id"; 
         }
 
         public static string GetParam(string nome, string apelido, string stack){
-            string query = @"SELECT * FROM PESSOA WHERE 1=1 ";
+            string query = "SELECT * FROM PESSOA WHERE 1=1 ";
             
             if(!string.IsNullOrWhiteSpace(nome))
-                query += @"AND NOME ILIKE @nome ";
+                query += "AND NOME ILIKE @nome ";
             else if(!string.IsNullOrWhiteSpace(apelido))
-                query += @"AND APELIDO ILIKE @apelido ";
+                query += "AND APELIDO ILIKE @apelido ";
             else if(!string.IsNullOrWhiteSpace(stack))
-                query += @"AND STACK ILIKE @stack ";
+                query += "AND STACK ILIKE @stack ";
             else
                 throw new ArgumentNullException();
             
-            query += @"LIMIT 50";
+            query += "LIMIT 50";
             return query;
         }
 
         public static string Count(){
-            return @"SELECT COUNT(1) FROM PESSOA";
+            return "SELECT COUNT(1) FROM PESSOA";
         }
 
         public static string Post(Pessoa pessoa){
-            return @"INSERT INTO PESSOA (ID, NOME, APELIDO, NASCIMENTO, STACK)" + 
-                    @"VALUES (@Id, @Nome, @Apelido, @Nascimento, @Stack) RETURNING ID";
+            return @"INSERT INTO PESSOA (ID, NOME, APELIDO, NASCIMENTO, STACK)
+                    VALUES (@Id, @Nome, @Apelido, @Nascimento, @Stack) RETURNING ID";
         }
     }
 }
