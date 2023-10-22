02fd41d66c546463318de344b310cf08825b9901
Raphsodyz <arthurvsr@gmail.com>
2023-10-22 16:34:22 -0300

    redis and pg search by id methods

--- src/Model/Pessoa.cs
+++ src/Model/Pessoa.cs
@@ -3,12 +3,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using System.Text.Json.Serialization;
 using Npgsql;
+using ServiceStack.Redis;
+using ServiceStack.Redis.Generic;
 
 
 namespace Model
 {
     public class Pessoa
     {
+        public Pessoa(Guid id, string nome, string apelido, DateOnly nascimento, string stack)
+        {
+            Id = id;
+            Nome = nome;
+            Apelido = apelido;
+            Nascimento = nascimento;
+            Stack = stack;
+        }
+
         [Key]
         [Required]
         [Column("ID")]
@@ -25,7 +36,7 @@ namespace Model
         public string Apelido { get; set; }
         
         [Column("NASCIMENTO")]
-        public DateOnly? Nascimento { get; set; }
+        public DateOnly Nascimento { get; set; }
         
         [Column("STACK")]
         public string Stack { get; set; }
@@ -39,7 +50,7 @@ namespace Model
                         throw new ValidationException();
         }
 
-        public static async Task BatchInsertPg(NpgsqlConnection connection, List<Pessoa> listaPessoa){
+        internal static async Task BatchInsertPg(NpgsqlConnection connection, List<Pessoa> listaPessoa){
             await connection.OpenAsync();
             using var trans = connection.BeginTransaction();
             
@@ -65,14 +76,42 @@ namespace Model
 
                 trans.Commit();
             }
-            catch(Exception ex){
-                string jaja = ex.Message;
+            catch(Exception){
                 trans.Rollback();
             }
             finally{
                 await connection.CloseAsync();
                 trans.Dispose();
             }
+        }
+        internal static async Task<Pessoa> FindByIdRedis(RedisClient client, Guid id){
+            IRedisTypedClient<Pessoa> redis = client.As<Pessoa>();
+            var redisList = redis.Lists["Pessoas"];
+            var pessoa = redisList.FirstOrDefault(p => p.Id == id);
+
+            return pessoa;
+        }
+        internal static async Task<Pessoa> FindByIdPostgres(NpgsqlConnection connection, Guid id){
+            await connection.OpenAsync();
+            using var cmd = connection.CreateCommand();
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.CommandText = "SELECT * FROM PESSOA WHERE ID = @id";
+            using var reader = await cmd.ExecuteReaderAsync();
+            while(await reader.ReadAsync()){
+                Pessoa pessoa = GetPessoa(reader);
+                return pessoa;
+            }
+            return null;
+        }
+        internal static Pessoa GetPessoa(NpgsqlDataReader reader){
+            Guid? Id = reader["Id"] as Guid?;
+            string Nome = reader["Nome"] as string;
+            string Apelido = reader["Apelido"] as string;
+            DateOnly? Nascimento = reader["Nascimento"] as DateOnly?;
+            string Stack = reader["Stack"] as string;
+
+            Pessoa pessoa = new(Id ?? Guid.Empty, Nome, Apelido, Nascimento ?? new DateOnly(), Stack);
+            return pessoa;
         }
     }
 }
